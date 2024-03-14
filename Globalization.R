bd8586eeb10b5b52bfc3e25d03326aeab6c5622d
Sergey*X <139458161+SergeyHSE@users.noreply.github.com>
2024-03-14 11:41:02 +0300

    Update Globalization.R

--- Globalization.R
+++ Globalization.R
@@ -60,3 +60,12 @@ write.csv(tidy(random), "random.csv")
 #Choice between RE and FE (Hausman test)
 phtest(fixed, random)
 
+#Time-FE
+fixed.time <- plm(Hungry ~ Export+Gini_coef+Import+TertierySchool+School+SecondarySchool + factor(Year), data=df, index=c("Country_Name", "Year"), model="within")
+summary(fixed.time)
+write.csv(tidy(fixed.time), "fixed_time.csv")
+
+#Choice time-FE and FE
+pFtest(fixed.time, fixed)
+plmtest(fixed, c("time"), type=("bp"))
+
